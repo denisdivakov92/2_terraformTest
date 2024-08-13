eadc057a39ee4dfdb53987a5933bb6e170b15c57
denisdivakov92 <denisdivakov92@gmail.com>
2024-08-13 11:26:15 -0700

    Initial commit

---
+++ provider.tf
@@ -0,0 +1,15 @@
+#Definition of a provider ==> https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.57.0"
+    }
+  }
+}
+
+#Provider Configuration
+provider "aws" {
+  profile = "terraform_denis" #Get profile from .aws/config
+  region  = "us-west-2"
+}
